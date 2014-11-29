8e8c9ee3eb6feeabf338a1244464f7179625d4eb
Eddie Antonio Santos <easantos@ualberta.ca>
2014-11-29 15:23:59 -0700

    Add token sample data.

---
+++ spec/fixtures/generic_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Some indents, dedents, half-completed, end of lines, etc.
+
+import os
+
+def thats_pretty_neat():
+    for neatness in generate_joy_and_happyness():
+        # It's not often you find all this neatness in one place.
+        print("Wow. Isn't %d neat?" % (neatness,))
+
+def generate_joy_and_happyness():
+    while thats_pretty_neat:
+        yield os.fork()
+
+# Intentionally incomplete.
+if __name__ ==
